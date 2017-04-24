af64c6115cb9b4044cb5ed0f905aa155716db812
jiaxin.li <vivianink@126.com>
2017-04-24 17:33:26 +0800

    update event

---
+++ js/event.js
@@ -0,0 +1,53 @@
+var EventUtil = {
+/**
+* 事件代理
+* @param {Node} delegator 在该元素上进行代理
+* @param {string} type 事件类型
+* @param {string} selector 要代理的元素选择器，目前IE11-仅支持id、class和标签名，其余浏览器支持合法的CSS选择器
+* @param {function} handler 事件处理函数
+**/
+    on: function(delegator, type, selector, handler) {
+        if (delegator.addEventListener) {
+            delegator.addEventListener(type, eventFunc);
+
+        } else if (delegator.attachEvent){
+            delegator.attachEvent('on' + type, eventFunc);
+
+        } else {
+            delegator['on' + type] = eventFunc;
+        }
+
+        function eventFunc(event) {
+            var event = event || window.event;
+            var target = event.target || event.srcElement;
+
+            if (typeof selector === 'function') { //绑定自身
+                var _handler = selector;
+                _handler.call(delegator, event);
+
+            } else { //代理
+                isTarget(target, selector) && handler && handler.call(target, event);
+            }
+        }
+
+        // 判断目标元素是否是被代理的元素
+        function isTarget(target, selector) {
+
+            if (document.querySelectorAll) {
+                var targets = Array.prototype.slice.call(delegator.querySelectorAll(selector));
+                return targets.indexOf(target) !== -1;
+            }
+
+            if (selector[0] === '#') {
+                return target.id === selector.slice(1);
+            }
+
+            if (selector[0] === '.') {
+                var clcName = selector.slice(1);
+                return target.className.indexOf(className) !== -1;
+            }
+
+            return target.nodeName.toLowerCase() === selector.toLowerCase();
+        }
+    }
+};
